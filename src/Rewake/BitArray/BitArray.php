e2930a7726a8336f52835a126d914a0e2f3e1160
Richard Komatz <r.komatz@gmail.com>
2021-12-13 22:32:43 -0500

    added comments, updated keys() for multi-row bit set

--- src/Rewake/BitArray/BitArray.php
+++ src/Rewake/BitArray/BitArray.php
@@ -6,9 +6,16 @@ class BitArray
 {
     public const BIT_LENGTH = PHP_INT_SIZE * 8;
 
+    // TODO: research SplFixedArray, Vector, etc. for container
     private array $container = [0];
     private int $data = 0;
 
+    /**
+     * Parses array of keys/indexes into a BitArray
+     *
+     * @param array $keys
+     * @return BitArray
+     */
     public function parse(array $keys): BitArray
     {
         foreach ($keys as $key) {
@@ -17,27 +24,54 @@ class BitArray
         return $this;
     }
 
+    /**
+     * Set the given bit (set to 1)
+     *
+     * @param int $bit
+     */
     public function set(int $bit): void
     {
         $this->locate($bit)->data |= (1 << $this->offset($bit));
     }
 
+    /**
+     * Unset the given bit (set to 0)
+     *
+     * @param int $bit
+     */
     public function unset(int $bit): void
     {
         $this->locate($bit)->data = ~(1 << $this->offset($bit));
     }
 
+    /**
+     * See if the given bit is enabled (is set to 1)
+     *
+     * @param int $bit
+     * @return bool
+     */
     public function enabled(int $bit): bool
     {
         return (1 << $this->offset($bit)) & $this->locate($bit)->data;
     }
 
+    /**
+     * Get row index of given bit
+     *
+     * @param int $bit
+     * @return int
+     */
     protected function row(int $bit): int
     {
         return intdiv($bit, self::BIT_LENGTH);
     }
 
-    // TODO: research: SplFixedArray, Vector
+    /**
+     * Locates the given bit by determining & referencing its row
+     *
+     * @param int $bit
+     * @return $this
+     */
     protected function locate(int $bit): BitArray
     {
         // Get row index
@@ -71,11 +105,12 @@ class BitArray
      */
     public function keys(): array
     {
+        $this->fillContainer();
         $keys = [];
-        foreach ($this->container as $row) {
+        foreach ($this->container as $index => $row) {
             $key = 0;
             do {
-                ! ($row & 1) ?: $keys[] = $key;
+                ! ($row & 1) ?: $keys[] = $key + ($index * self::BIT_LENGTH);
                 if ($row == -1) break;
                 $key++;
             } while ($row >>= 1);
@@ -83,7 +118,14 @@ class BitArray
         return $keys;
     }
 
-    public function fillContainer()
+    /**
+     * Fills the container rows up to max bits' row
+     * Ex:  if only bit 64 is set (in row[1]), row[0] would not be
+     *      created, so this method "fills" the empty container rows.
+     *
+     * @return void
+     */
+    public function fillContainer(): void
     {
         $max = max(array_keys($this->container));
         $current = 0;
--- test.php
+++ test.php
@@ -5,13 +5,13 @@ use Rewake\BitArray\BitArray;
 require_once 'vendor/autoload.php';
 
 
-$selectedOptions = [33, 17, 10, 12, 24];
+$selectedOptions = [0, 63, 64, 1337];
 
 
 $options = new BitArray();
 $options->parse($selectedOptions);
 
-echo $options->binary(), PHP_EOL;
-echo $options->int(), PHP_EOL;
+//echo $options->binary(), PHP_EOL;
+//echo $options->int(), PHP_EOL;
 echo var_export($options->keys(), true), PHP_EOL;
-echo var_export($options->toArray(), true), PHP_EOL;
+//echo var_export($options->toArray(), true), PHP_EOL;
